acfd478afdd718cb2afb1ea56a89c26f51bdfed2
mihaita.tinta <mihaita.tinta@ing.com>
2024-11-28 16:24:36 +0200

    add tracing wip

--- sample-app/src/main/java/tech/duo/app/http/RestTemplateConfig.java
+++ sample-app/src/main/java/tech/duo/app/http/RestTemplateConfig.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.httpcomponents.hc5.PoolingHttpClientConnectionManagerMetricsBinder;
+import io.opentelemetry.api.OpenTelemetry;
+//import io.opentelemetry.instrumentation.spring.web.v3_1.SpringWebTelemetry;
+//import io.opentelemetry.instrumentation.spring.webmvc.v5_3.SpringWebMvcTelemetry;
+import jakarta.servlet.Filter;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -26,8 +31,11 @@ public class RestTemplateConfig {
     @Value("${rest-template.connections.per-route-max-total:100}")
     int perRouteMaxTotal;
 
+//    @Autowired
+//    SpringWebTelemetryBuilder otelBuilder;
+
     @Bean
-    RestTemplate restTemplate(RestTemplateBuilder builder, MeterRegistry registry) {
+    RestTemplate restTemplate(RestTemplateBuilder builder, MeterRegistry registry, OpenTelemetry openTelemetry) {
 
         PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
         poolingHttpClientConnectionManager.setDefaultMaxPerRoute(perRouteMaxTotal);
@@ -49,6 +57,7 @@ public class RestTemplateConfig {
                 HttpComponentsClientHttpRequestFactory();
         httpRequestFactory.setHttpClient(client);
         return builder
+//                .interceptors(SpringWebTelemetry.create(openTelemetry).newInterceptor())
                 .requestFactory(() -> httpRequestFactory)
                 .build();
     }
@@ -60,4 +69,5 @@ public class RestTemplateConfig {
 //            customizer.featuresToDisable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
         };
     }
+
 }
